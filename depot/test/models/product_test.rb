4b099f5d1f173f4e04f7e92b60047259437705a6
Andrey Kravets <andrew.kravets@yahoo.com>
2014-07-20 14:26:23 +0300

    Store controller added Root link changed to store controller

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -62,9 +62,9 @@ class ProductTest < ActiveSupport::TestCase
 
   test "product is not valid without a unique title - i18n" do
     product = Product.new(title: products(:ruby).title,
-                        description: "yyy",
-                        price: 1,
-                        image_url: "fred.gif")
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
 
     assert product.invalid?
     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
