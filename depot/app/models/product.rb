53591638969e965f964d04a910b435b240447cbe
Andrew Kravets <andrew.kravets@yahoo.com>
2014-07-22 16:25:36 +0300

    Cart added

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'There is a product position')
+      false
+    end
+  end
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
