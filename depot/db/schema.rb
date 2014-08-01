c6fd49bc9d21b798fe47ea4338a40f71606e5c77
Andrew Kravets <andrew.kravets@yahoo.com>
2014-08-01 17:57:25 +0300

    Cart exception handler added

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722133059) do
+ActiveRecord::Schema.define(version: 20140725125855) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
